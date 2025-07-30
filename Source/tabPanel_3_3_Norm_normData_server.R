0d23c5d8ff7575ee4f6f42dc8c146cce2f7ab115
job_eccc-admin <jason.obrien@ec.gc.ca>
2025-07-30 15:56:01 -0400

    normData_download
    dowload normalized data tab is now functional

--- Source/tabPanel_3_3_Norm_normData_server.R
+++ Source/tabPanel_3_3_Norm_normData_server.R
@@ -1,4 +1,65 @@
 tabPanel_3_3_Norm_normData_server <- function(input, output, session, shared){
   
+  # COMPUTE ----
+  
+  
+  # OUTPUT ----
+  output$normalizedData <- renderReactable({
+    req(shared$normData())
+    
+    normData <- shared$normData()
+    
+    GOIs <- names(normData)[names(normData)%in%shared$geneList()]
+    
+    reactable(
+      normData,
+      defaultColDef = colDef(
+        style = function(value) { # to highlight NAs (No Ct) red
+          if (is.na(value)) {
+            color <- failCol
+          } else {
+            color <- passCol
+          }
+          list(color = color, fontWeight = "lighter")
+        },
+        na="no Ct",
+        minWidth = 85,
+        align = "center",
+        format = colFormat(digits = 1)
+      ),
+      columns = list(
+        sample = colDef(minWidth = 100, align="left"),
+        chemical = colDef(align="left")
+      ),
+      fullWidth = TRUE,
+      showPageSizeOptions = TRUE,
+      pageSizeOptions = c(10, 25, 50, 100)
+    )
+  })
+  
+  # DYNAMICS ----
+  
+  # DOWNLOAD NORM DATA WHEN AVAILABLE
+  observe({
+    if(!is.null(shared$normData())){
+      output$downloadNormData <- downloadHandler(
+        filename = function(){
+          "normalizedData.tsv"
+        },
+        content = function(file){
+          write.table(shared$normData(), file, sep="\t", row.names = FALSE)
+        }
+      )
+    } else {
+      output$downloadNormData <- NULL
+    }
+  })
+  
+  # To DEG Button
+  observeEvent(
+    input$toDEGs, {
+      updateTabsetPanel(inputId = "topTab", selected = "DEG analysis")
+    })
+  
   
 }
--- Source/tabPanel_3_3_Norm_normData_ui.R
+++ Source/tabPanel_3_3_Norm_normData_ui.R
@@ -1,3 +1,30 @@
 tabPanel_3_3_Norm_normData_ui <- tabPanel(
-  "Normalized Data"
+  "Normalized Data",
+  fluidRow(
+    column(
+      12,
+      h3("Normalized Data"),
+      "Here, you can download a table of the normalized data",
+      br(),br()
+    )
+  ),
+  fluidRow(
+    column(
+      2,
+      h4("Download Normalized Data"),
+      "Click below to download the normalized data",
+      br(),
+      downloadButton("downloadNormData", "Download Normalized Data"),
+      br(),br(),
+      
+      actionButton("toDEGs", "Proceed to DEG analysis", class= "btn-success", disabled = FALSE),
+      br(),br()
+    ),
+    column(
+      10,
+      h3("Preview of Normalized Data"),
+      br(),
+      reactableOutput("normalizedData")
+    )
+  )
 )
